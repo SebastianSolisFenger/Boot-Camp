4b10630cae9283e8131550e17bf3834343bf6d14
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-04-12 23:15:51 -0300

    Fix stylelint js

--- JavaScript/App.js
+++ JavaScript/App.js
@@ -1,11 +1,9 @@
 const hamburgerBtn = document.querySelector('.hamb-toggle');
 const mainMenu = document.querySelector('.menu-box');
-// const ulList = document.querySelector(".list");
 
 hamburgerBtn.addEventListener('click', () => {
   mainMenu.classList.toggle('active');
   hamburgerBtn.classList.toggle('active');
-  // ulList.classList.toggle("activeV");
 });
 
 const proffesionals = [
